3aec1954320ccabdc01c29bae24f742443a0cbe0
Andrea Ercolino <cappuccino.e.cornetto@gmail.com>
2021-09-30 20:46:49 +0200

    Replace "HelloWorld" with basic component for lottery results
    Get relevant data from sample response

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <ae-results></ae-results>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import AeResults from './components/AeResults.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld
+    AeResults
   }
 }
 </script>
---
+++ src/components/AeResults.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+  </div>
+</template>
+
+<script>
+import responseData from '../../dev-documents/sample-response.json'
+const lastDraw = responseData.last;
+
+export default {
+  name: "AeResults",
+  data() {
+    return {
+      apiDate: lastDraw.date,
+      apiNumbers: lastDraw.numbers,
+      apiEuroNumbers: lastDraw.euroNumbers,
+      apiOdds: lastDraw.odds,
+    };
+  },
+};
+</script>
+
+<style scoped>
+</style>
