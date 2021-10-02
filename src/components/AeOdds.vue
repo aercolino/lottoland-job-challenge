92eb922ad19e553b6097f48da0ced6613d32062b
Andrea Ercolino <cappuccino.e.cornetto@gmail.com>
2021-10-02 12:41:28 +0200

    Add basic table

--- src/components/AeOdds.vue
+++ src/components/AeOdds.vue
@@ -1,16 +1,45 @@
 <template>
   <div>
     <p>ranks: {{ ranks }}</p>
+    <table width="100%">
+      <tr>
+        <th width="10%">
+          Tier
+        </th>
+        <th width="30%">
+          Match
+        </th>
+        <th width="30%">
+          Winners
+        </th>
+        <th width="30%">
+          Amount
+        </th>
+      </tr>
+      <tr>
+        <td>
+          Tier
+        </td>
+        <td>
+          Match
+        </td>
+        <td>
+          Winners
+        </td>
+        <td>
+          Amount
+        </td>
+      </tr>
+    </table>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'AeOdds',
-  props: ['ranks']
-}
+  name: "AeOdds",
+  props: ["ranks"],
+};
 </script>
 
 <style scoped>
-
 </style>
